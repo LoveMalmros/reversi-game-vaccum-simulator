a037c40dc78701eb5c28393bb00aa97468163335
kingbabo <love.malmros@gmail.com>
2019-02-28 17:58:01 +0100

    preceptron

--- perceptron.py
+++ perceptron.py
@@ -7,10 +7,13 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 
+ENGLISH_CLASSIFIER = 0
+FRENCH_CLASSIFIER = 1
+
 # divides English and French to two different classes: 0 & 1.
 y_train = np.array(
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    [ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER, ENGLISH_CLASSIFIER,
+     FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER, FRENCH_CLASSIFIER])
 
 X_train = np.array(
     [[35680, 2217], [42514, 2761], [15162, 990], [35298, 2274],
@@ -40,17 +43,17 @@ y_test_predicted = classifier.predict(X_train)
 # fetch weights
 # w0 = y_test_predicted[0::2]
 # w1 = y_test_predicted[1::2]
-def parse_sparse_format():
-    format_string = '0 '
-    for x in X_train[:15]:
+def parse_sparse_format(x_data):
+    format_string = ''
+    for x in x_data[:15]:
+        format_string += '0 '
         format_string += '1:' + str(x[0]) + ' '
-        format_string += '2:' + str(x[1]) + ' '
-    format_string += '\n1 '
-    for x in X_train[15:]:
+        format_string += '2:' + str(x[1]) + '\n'
+    for x in x_data[15:]:
+        format_string += '1 '
         format_string += '1:' + str(x[0]) + ' '
-        format_string += '2:' + str(x[1]) + ' '
-    return format_string
-print(parse_sparse_format())
+        format_string += '2:' + str(x[1]) + '\n'
+    return format_string[:-1]
 
 def compute_error_for_line_given_points(b, m, points):
     totalError = 0
@@ -96,93 +99,46 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
             b, m = batch_step_gradient(b, m, points, learning_rate, j*3, 3)
     return [b, m]
 
-# stochastic gradient descent create weights w0 and w1: y_pred = w1 * x + w0
-def stochastic_gradient_descent(x, y, error, alpha):
-    # init weights
-    w0 = 0.001
-    w1 = 0.001
-    e = 0.001
-    w0_list = []
-    w1_list = []
-    e_list = []
-    new_weights = stochastic_sum(x[0], y[0], w0, w1, alpha)
+def linear_classifier(b, m, x1, x2):
+    print(m*x1 + b - x2)
+    if m*x1 + b - x2 > 0:
+        return ENGLISH_CLASSIFIER
+    else: 
+        return FRENCH_CLASSIFIER
 
-    print('Initialized new weights: ' + str(new_weights))
-    counter = 0
+def reader_sparse_data(data):
+    data_split = data.split(' ')
+    classifier = data_split[0]
+    x1 = data_split[1].split(':')[1]
+    x2 = data_split[2].split(':')[1]
+    return classifier, x1, x2
 
-    for i in range(100):
-        w0 += new_weights[0]
-        w1 += new_weights[1]
-        e += new_weights[2]
-        w0_list.append(new_weights[0])
-        w1_list.append(new_weights[1])
-        e_list.append(new_weights[2])
-        #print('new w0 and w1: ' + str(w0) + '  ' + str(w1))
-
-        new_weights = stochastic_sum(x[counter], y[counter], w0, w1, alpha)
-        # w0, w1 = ... instead?
-
-        if counter == len(x)-1:
-            counter == 0
-        else:
-            counter += 1
-    return w0_list, w1_list, e_list
-
-
-def stochastic_sum(x, y, w0, w1, alpha):
-    new_w0 = w0  # init with 0 instead?
-    new_w1 = w1
-
-    # total squared error
-    sqerror = 0
-
-    # error for specific x, y
-    error = 0
-
-    # predicted y = f(x)
-    yhat = 0
-
-    # single squared error
-    se = 0
-
-    # update weights
-    yhat = w1 * x + w0
-    error = y - yhat
-    se = error ** 2  # ** means 'power of'
-    sqerror += se
-    new_w0 = alpha * error
-    new_w1 = alpha * error * x
-
-    return new_w0, new_w1, sqerror
-
-
-# divide dataset into corresponding language and sort
-"""
-en_x = X_train[:15, 0]
-en_y = X_train[:15, 1]
-
-fr_x = X_train[15:, 0]
-fr_y = X_train[15:, 1]
-# alpha = 0.025  # learning rate
-alpha = 1e-5  # used when not normalized
-error = 1e-10  # use lower value for normalized data
-
-w0, w1, e = stochastic_gradient_descent(norm_en_x, norm_en_y, error, alpha)
-
-w0_fr, w1_fr, e_fr = stochastic_gradient_descent(fr_x, fr_y, error, alpha)
-"""
+def leave_one_out_validation(sparse_data):
+    data = sparse_data.split('\n')
+    for i, line in enumerate(data):
+        temp_norm_X = np.delete(norm_X, i, 0)
+        [b, m] = gradient_descent_runner(temp_norm_X, 0, 0, 1e-3, 5000, True)
+        [c, x1, x2] = reader_sparse_data(line)
+        print(int(c) == linear_classifier(b, m, float(x1), float(x2)))
 
 
 norm_en_x = norm_X[:15, 0]
-norm_en_y = norm_X[:15, 1]
 norm_fr_x = norm_X[15:, 0]
+norm_en_y = norm_X[:15, 1]
 norm_fr_y = norm_X[15:, 1]
+norm_x = norm_X[:, 0]
+leave_one_out_validation(parse_sparse_format(norm_X))
+
 [b_en, m_en] = gradient_descent_runner(norm_X[:15], 0, 0, 1e-1, 1000, True)
 [b_fr, m_fr] = gradient_descent_runner(norm_X[15:], 0, 0, 1e-1, 1000, True)
-[b_en2, m_en2] = gradient_descent_runner(norm_X[:15], 0, 0, 1e-1, 1000, False)
-[b_fr2, m_fr2] = gradient_descent_runner(norm_X[15:], 0, 0, 1e-1, 1000, False)
-print(b_en)
-print(m_en)
+
+##BATCH
+#[b_en2, m_en2] = gradient_descent_runner(norm_X[:15], 0, 0, 1e-1, 1000, False)
+#[b_fr2, m_fr2] = gradient_descent_runner(norm_X[15:], 0, 0, 1e-1, 1000, False)
+
+
+[b, m] = gradient_descent_runner(norm_X, 0, 0, 1e-3, 5000, True)
+
 """
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -197,5 +153,6 @@ plt.xlabel('Number of characters')
 plt.ylabel('Number of A:s')
 plt.plot(norm_en_x, m_en*norm_en_x + b_en, 'g--')
 plt.plot(norm_fr_x, m_fr*norm_fr_x + b_fr, 'b--')
+plt.plot(norm_x, m*norm_x + b, 'r--')
 #plt.plot(fr_x, w1_fr*fr_x + w0_fr, 'b--')
 plt.show()
